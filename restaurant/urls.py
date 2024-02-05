248d4f10209945b070d3377513a69e0728dbd8e8
pili-zhangqiu <p.zhangqiu@gmail.com>
2024-02-05 09:48:07 +0000

    feat(menu-item-page): Create subpage for each menu item & add hyperlink

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('about/', views.about, name="about"),
     path('book/', views.book, name="book"),
     path('menu/', views.menu, name="menu"),
+    path('menu_item/<int:pk>/', views.display_menu_item, name="menu_item"),
 ]
